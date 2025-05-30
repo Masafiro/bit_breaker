b02f22c771a08ff95a5d5ea2bce49ed7596d412b
Masafiro <141376324+Masafiro@users.noreply.github.com>
2025-05-30 18:19:20 +0900

    vercel speed insights を有効にした

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights />
         {children}
       </body>
     </html>
