e3e965d1cb92f3078783b7a70f7fcdfcad0955a3
Manato Suzuki <62659243+Katu2ou@users.noreply.github.com>
2025-06-09 22:44:15 +0900

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,13 @@
-// export default nextConfig;
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'lh3.googleusercontent.com',
-    //     port: '',
-    //     pathname: '/**',
-    //   },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
@@ -23,7 +19,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information. 
